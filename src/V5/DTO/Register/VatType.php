c6a209aad4b8ed2667e678e1c21c32d122049550
Dmitry Matvienko <dmitry.matvienko@lamoda.ru>
2021-09-21 15:34:10 +0300

    Add VatType 20 and 120

--- src/V5/DTO/Register/VatType.php
+++ src/V5/DTO/Register/VatType.php
@@ -11,8 +11,10 @@ use Paillechat\Enum\Enum;
  * @method static self VAT0()
  * @method static self VAT10()
  * @method static self VAT18()
+ * @method static self VAT20()
  * @method static self VAT110()
  * @method static self VAT118()
+ * @method static self VAT120()
  */
 final class VatType extends Enum
 {
@@ -20,6 +22,8 @@ final class VatType extends Enum
     protected const VAT0 = 'vat0';
     protected const VAT10 = 'vat10';
     protected const VAT18 = 'vat18';
+    protected const VAT20 = 'vat20';
     protected const VAT110 = 'vat110';
     protected const VAT118 = 'vat118';
+    protected const VAT120 = 'vat120';
 }
