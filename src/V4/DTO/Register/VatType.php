835808d7ea1e39f619a5473d45a1e84cd4c7aba3
AdamSolaris <ondbarn@gmail.com>
2024-12-16 14:25:52 +0300

    Add vat5 and vat7 enums

--- src/V4/DTO/Register/VatType.php
+++ src/V4/DTO/Register/VatType.php
@@ -9,6 +9,8 @@ use Paillechat\Enum\Enum;
 /**
  * @method static self NONE()
  * @method static self VAT0()
+ * @method static self VAT5()
+ * @method static self VAT7()
  * @method static self VAT10()
  * @method static self VAT18()
  * @method static self VAT20()
@@ -20,6 +22,8 @@ final class VatType extends Enum
 {
     protected const NONE = 'none';
     protected const VAT0 = 'vat0';
+    protected const VAT5 = 'vat5';
+    protected const VAT7 = 'vat7';
     protected const VAT10 = 'vat10';
     protected const VAT18 = 'vat18';
     protected const VAT20 = 'vat20';
