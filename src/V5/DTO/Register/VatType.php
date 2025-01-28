6911c2c6868a38b3743b879d0266237af2f3cefe
Nikita Kinash <nikita.kinash@lamoda.ru>
2025-01-28 13:06:40 +0300

    Add new vats according to 2025 laws

--- src/V5/DTO/Register/VatType.php
+++ src/V5/DTO/Register/VatType.php
@@ -9,9 +9,13 @@ use Paillechat\Enum\Enum;
 /**
  * @method static self NONE()
  * @method static self VAT0()
+ * @method static self VAT5()
+ * @method static self VAT7()
  * @method static self VAT10()
  * @method static self VAT18()
  * @method static self VAT20()
+ * @method static self VAT105()
+ * @method static self VAT107()
  * @method static self VAT110()
  * @method static self VAT118()
  * @method static self VAT120()
@@ -20,9 +24,13 @@ final class VatType extends Enum
 {
     protected const NONE = 'none';
     protected const VAT0 = 'vat0';
+    protected const VAT5 = 'vat5';
+    protected const VAT7 = 'vat7';
     protected const VAT10 = 'vat10';
     protected const VAT18 = 'vat18';
     protected const VAT20 = 'vat20';
+    protected const VAT105 = 'vat105';
+    protected const VAT107 = 'vat107';
     protected const VAT110 = 'vat110';
     protected const VAT118 = 'vat118';
     protected const VAT120 = 'vat120';
